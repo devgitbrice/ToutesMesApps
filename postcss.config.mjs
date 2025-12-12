e828ab2aba21f9e351f2a6916aedf798829758b2
devgitbrice <dev.git@bricematter.com>
2025-12-12 23:15:29 +0000

    Fix: réparation de la configuration Tailwind et du style

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {}, // <--- C'est ici que ça change
+    autoprefixer: {},
   },
 };
 

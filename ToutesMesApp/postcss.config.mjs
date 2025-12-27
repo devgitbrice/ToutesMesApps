423d15561827c3667470dbc2ddc06301c21106d7
devgitbrice <dev.git@bricematter.com>
2025-12-27 15:44:56 +0000

    Suppression du dossier doublon ToutesMesApp qui bloque le build

--- ToutesMesApp/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {}, // <--- C'est ici que ça change
-    autoprefixer: {},
-  },
-};
-
-export default config;
